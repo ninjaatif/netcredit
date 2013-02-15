b49735017beb010a036fce3d2a31f4b633be2013
ATIF SIDDIQI <siddiqi28@ATIF-SIDDIQIs-MacBook.local>
2013-02-15 01:01:37 -0800

    Start rails app with status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Netcredit::Application.config.secret_token = '1215830b1b8857e76c737800482a8ffffb94d5451d59513a7babce3dccc2e911cb2e96e33c8e4e999723a6b704ec10b41c9c4640bd6214aeb848fe5859949271'
